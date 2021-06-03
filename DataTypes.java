5cc868630a152de1be3c35d1cadf41fcaefd30fe
Norvanryan Royansyah <ryanjaa9@gmail.com>
2021-06-04 01:46:16 +0800

    deklarasi variabel dan tipe data

---
+++ DataTypes.java
@@ -0,0 +1,14 @@
+public class DataTypes{
+	public static void main(String[]args){
+
+		String  namaDepan  =  "Norvan"; 
+		String  namaTengah  =  "ryan"; 
+		String  namaBelakang  =  "Royansyah"; 
+		
+		int  usia  =  19; 
+		int  targetTahunKuliah  =  4; 
+		double  ipk  =  3.89764512; 
+		char  nilaiAbjad  =  'A'; 
+		
+	}
+}
